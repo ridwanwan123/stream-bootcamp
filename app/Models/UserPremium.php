1f7c4d05203caf22afe8ca2825a52bf119cf91ea
Muhamad Ridwan <ridwanwan784@gmail.com>
2022-04-10 14:46:20 +0700

    commit

---
+++ app/Models/UserPremium.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserPremium extends Model
+{
+    use HasFactory;
+
+    protected $table = 'user_premiums';
+
+    protected $guarded = ['id'];
+}
